5c23bb0a27e81615c61a918560927f2256246694
NeeleshTammana <neeleshtammana05@gmail.com>
2023-05-03 15:34:52 +0530

    Commited

---
+++ src/Component/Form.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import TodoList from './List'
+export default class Form extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       inputData:"",
+       todoItems:['Do Exercise','hello']
+    }
+  }
+  changeTodoInput=(event)=>{
+    this.setState({inputData:event.target.value})
+    console.log(this.state.inputData)
+  }
+  handleSubmit=(event)=>{
+    if(this.state.inputData!==''){
+      let newItem= [...this.state.todoItems,this.state.inputData]
+      this.setState({
+        todoItems:newItem,
+        inputData:""
+      })
+    }
+  }
+  deleteItem=(index)=>{
+    //console.log("Button clicked")
+    let original=[...this.state.todoItems];
+    let left=original.filter((value,key)=>{
+      return index!==key
+    })
+    this.setState({
+      original:left
+    })
+  }
+  render() {
+    return (
+      <>
+      <div className='container my-3'>
+      <div className='row justify-content-center'>
+      <div className='col-md-10' style={{border:"2px solid",borderRadius:"50px",padding:"30px",backgroundColor:"#fff",color:"#fff"}}>
+    
+       <form onSubmit={this.handleSubmit}>
+        <div className="mb-3">
+    <h1>Todo app</h1>
+    <input type="text" className="form-control" id="todo" placeholder='Try typing do exercise' onChange={this.changeTodoInput} value={this.state.inputData} />   
+  </div>
+  <button type="button" className="btn btn-primary w-100" style={{borderRadius:"50px",padding:"5px"}}> 
+  
+      <span onClick={this.handleSubmit}>Add</span>
+  
+    </button>
+
+  </form>
+<TodoList items={this.state.todoItems} deleteItem={this.deleteItem}/>
+</div>
+</div>
+</div>
+      </>
+    )
+  }
+}
